507d3fc5737450e31d9840c5e46b28f995d825a5
Ryan Wallace <ryanwallace024@gmail.com>
2020-10-21 11:07:32 -0400

    intents added

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,19 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=".", intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name='getvotes')
+async def getVotes(ctx):
+    mems = discord.utils.get(ctx.guild.channels, id=759489613260783670)
+    print(mems.members)
 
-client.run(TOKEN)
+bot.run(TOKEN)
